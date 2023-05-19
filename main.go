25b99cc5db1dcd65607f287521a658646f2bbd63
zhanghaoran <2017929959@qq.com>
2023-05-19 15:30:39 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+)
+
+func main() {
+	serverConfigs := []constant.ServerConfig{
+		{
+			Scheme:      "http",
+			ContextPath: "/nacos",
+			IpAddr:      "127.0.0.1",
+			Port:        8848,
+		},
+	}
+	clientConfig := constant.ClientConfig{
+		NamespaceId:         "",
+		TimeoutMs:           10 * 1000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "/tmp/nacos/log",
+		CacheDir:            "/tmp/nacos/cache",
+	}
+	namingClient, err := clients.NewNamingClient(
+		vo.NacosClientParam{
+			ClientConfig:  &clientConfig,
+			ServerConfigs: serverConfigs,
+		})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	serviceName := "judge"
+	ip := "127.0.0.1"
+	port := 8888
+	// 注册一个实例
+	instance := vo.RegisterInstanceParam{
+		Ip:          ip,
+		Port:        uint64(port),
+		ServiceName: serviceName,
+	}
+	success, err := namingClient.RegisterInstance(instance)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(success)
+}
